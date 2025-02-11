6ada63d8a79f38608c9be1fb242bd98d6f3f9ef9
Dorian Virrueta <mr.dorianv@gmail.com>
2025-02-11 09:53:20 -0800

    Add comments to help understand equation
    I added a few comments to help myself understand
    part of the math.random equation and added some
    extra comments to use for future reference.

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ console.log("Rock Paper Scissors!")
 
 function getComputerChoice() {
     // Create a variable to hold the function equation
+    // The equation below generates a random number between 0 and 3 (excluding 3) using math.random
+    // then is rounded to the lowest integer (still excluding 3). 
+            
+            // If you wanted to include 3 it would look like this = Math.floor(Math.random() * 3) + 1
+            // Some other examples for future reference:
+            // 5 -> 20 (Generate a random number between 5 and 20)
+            // Math.floor(Math.random() * 16) + 5;
+            // -10 -> (-2) (Generate a random integer between -10 and -2)
+            // Math.floor(Math.random() * 9) - 10;
+
     let randomNumber = Math.floor(Math.random() * 3);
     // Create conditional statement to produce string options
     if (randomNumber === 0) {
