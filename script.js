1e936e8aa3c9ac01805f2c9b73cf550aaf1b4df1
Dorian Virrueta <mr.dorianv@gmail.com>
2025-02-11 09:34:57 -0800

    Add function to get computer choice
    I created the initial function to generate a random
    choice from the computer from the rock, paper, and
    scissors options. I tried to exclusively use the
    math.random function for this although I found many
    sources using arrays. I already struggled with
    this portion of the project.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// This is just to test the js file is connected to the html file. 
 console.log("Rock Paper Scissors!")
+// Write a function to return "Rock," "Paper," or "Scissors"
+
+
+function getComputerChoice() {
+    // Create a variable to hold the function equation
+    let randomNumber = Math.floor(Math.random() * 3);
+    // Create conditional statement to produce string options
+    if (randomNumber === 0) {
+        computerChoice = "Rock";
+    } else if (randomNumber === 1) {
+        computerChoice = "Paper";
+    } else if (randomNumber === 2) {
+        computerChoice = "Scissors";
+    }
+    return computerChoice;
+}
+console.log(getComputerChoice());
